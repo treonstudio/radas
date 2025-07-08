95fa2ef3e8a502a740b3bbe8fae54ef285f68f64
ridhoassuryadi <ulunmuhammadridho@gmail.com>
2025-07-08 16:35:28 +0700

    feat: update layout

--- apps/web/app/blog/layout.tsx
+++ apps/web/app/blog/layout.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
 	title: "Blog - RADAS",
-	description: "Latest updates, articles, and insights about RADAS,
+	description: "Latest updates, articles, and insights about RADAS",
 };
 
 interface BlogLayoutProps {
